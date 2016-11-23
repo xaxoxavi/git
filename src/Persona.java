caaefddfe9321744e02794a99dee13a0c52b31ad
xaxoxavi <xtorrens@esliceu.com>
2016-11-23 17:51:50 +0100

    Commit incial classe Persona

---
+++ src/Persona.java
@@ -0,0 +1,37 @@
+/**
+ * @author Xavi Torrens
+ */
+public class Persona {
+
+    private String dni;
+    private String nom;
+    private String cognom;
+
+    public Persona() {
+    }
+
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+}
