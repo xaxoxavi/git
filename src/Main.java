5825c2fb7fc143228d04a9925ad9fe199979f675
xaxoxavi <xtorrens@esliceu.com>
2016-11-23 17:47:30 +0100

    Commit incial

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.
-                println("Hello World!");
+        System.out.println("Hello World!");
 
+        List<String> llista = new ArrayList<String>();
 
     }
 }
